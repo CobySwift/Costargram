73ec04562d6ff002cd6129d78c9a0e6ba3110891
coby5502 <coby5502@gmail.com>
2023-02-20 14:10:31 +0900

    Initial Commit

---
+++ Costargram/CostargramApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostargramApp.swift
+//  Costargram
+//
+//  Created by Coby Kim on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct CostargramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
