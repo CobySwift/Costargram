bc9f3ce14765bdfe139ae17b975dff3ef0fb9fd4
coby5502 <coby5502@gmail.com>
2023-02-21 17:19:14 +0900

    Part 2

--- Costargram/View/Login/RegisterView.swift
+++ Costargram/View/Login/RegisterView.swift
@@ -1,8 +1,8 @@
 //
-//  LoginView.swift
+//  RegisterView.swift
 //  Costargram
 //
-//  Created by Coby Kim on 2023/02/20.
+//  Created by Coby Kim on 2023/02/21.
 //
 
 import SwiftUI
@@ -11,136 +11,6 @@ import Firebase
 import FirebaseFirestore
 import FirebaseStorage
 
-struct LoginView: View {
-    // MARK: User Details
-    @State var emailID: String = ""
-    @State var password: String = ""
-    // MARK: View Properties
-    @State var createAccount: Bool = false
-    @State var showError: Bool = false
-    @State var errorMessage: String = ""
-    @State var isLoading: Bool = false
-    // MARK: UserDefaults
-    @AppStorage("log_status") var logStatus: Bool = false
-    @AppStorage("user_profile_url") var profileURL: URL?
-    @AppStorage("user_name") var userNameStored: String = ""
-    @AppStorage("user_UID") var userUID: String = ""
-    var body: some View {
-        VStack(spacing: 10) {
-            Text("Lets Sign you in")
-                .font(.largeTitle.bold())
-                .hAlign(.leading)
-            
-            Text("Welcome Back,\nYou have missed")
-                .font(.title3)
-                .hAlign(.leading)
-            
-            VStack(spacing: 12) {
-                TextField("Email", text: $emailID)
-                    .textContentType(.emailAddress)
-                    .border(1, .gray.opacity(0.5))
-                    .padding(.top, 25)
-                
-                SecureField("Password", text: $password)
-                    .textContentType(.emailAddress)
-                    .border(1, .gray.opacity(0.5))
-                
-                Button("Reset password?", action: resetPassword)
-                    .font(.callout)
-                    .fontWeight(.medium)
-                    .tint(.black)
-                    .hAlign(.trailing)
-                
-                Button(action: loginUser) {
-                    // MARK: Login Button
-                    Text("Sign in")
-                        .foregroundColor(.white)
-                        .hAlign(.center)
-                        .fillView(.black)
-                }
-                .padding(.top, 10)
-            }
-            
-            // MARK: Register Button
-            HStack {
-                Text("Don't have an account?")
-                    .foregroundColor(.gray)
-                
-                Button("Register Now") {
-                    createAccount.toggle()
-                }
-                .fontWeight(.bold)
-                .foregroundColor(.black)
-            }
-            .font(.callout)
-            .vAlign(.bottom)
-        }
-        .vAlign(.top)
-        .padding(15)
-        .overlay(content: {
-            LoadingView(show: $isLoading)
-        })
-        // MARK: Register View VIA Sheets
-        .fullScreenCover(isPresented: $createAccount) {
-            RegisterView()
-        }
-        // MARK: Displaying Alert
-        .alert(errorMessage, isPresented: $showError, actions: {})
-    }
-    
-    func loginUser() {
-        isLoading = true
-        closeKeyboard()
-        Task {
-            do {
-                // With the help of Swift Concurrency Auth can be done with Single Line
-                try await Auth.auth().signIn(withEmail: emailID, password: password)
-                print("User Found")
-                try await fetchUser()
-            } catch {
-                await setError(error)
-            }
-        }
-    }
-    
-    // MARK: If User if Found then Fetching User Data From Firestore
-    func fetchUser() async throws {
-        guard let userID = Auth.auth().currentUser?.uid else { return }
-        let user = try await Firestore.firestore().collection("Users").document(userID).getDocument(as: User.self)
-        // MARK: UI Updating Must be Run On Main Thread
-        await MainActor.run(body: {
-            // Setting UserDefaults data and Changing App's Auth Status
-            self.userNameStored = user.username
-            self.userUID = user.userUID
-            self.profileURL = user.userProfileURL
-            self.logStatus = true
-        })
-    }
-    
-    func resetPassword() {
-        Task {
-            do {
-                // With the help of Swift Concurrency Auth can be done with Single Line
-                try await Auth.auth().sendPasswordReset(withEmail: emailID)
-                print("Link Sent")
-            } catch {
-                await setError(error)
-            }
-        }
-    }
-    
-    // MARK: Displaying Errors VIA Alert
-    func setError(_ error: Error) async {
-        // MARK: UI Must be Updated on Main Thread
-        await MainActor.run(body: {
-            errorMessage = error.localizedDescription
-            showError.toggle()
-            isLoading = false
-        })
-    }
-}
-
-// MARK: Register View
 struct RegisterView: View {
     // MARK: User Details
     @State var emailID: String = ""
@@ -316,58 +186,5 @@ struct RegisterView: View {
             showError.toggle()
             isLoading = false
         })
-    }
-}
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
-}
-
-// MARK: View Extensions For UI Building
-extension View {
-    // Closing All Active Keyboards
-    func closeKeyboard() {
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
-    
-    // MARK: Disabling with Opacity
-    func disableWithOpacity(_ condition: Bool) -> some View {
-        self
-            .disabled(condition)
-            .opacity(condition ? 0.6 : 1)
-    }
-    
-    func hAlign(_ alignment: Alignment) -> some View {
-        self
-            .frame(maxWidth: .infinity, alignment: alignment)
-    }
-    
-    func vAlign(_ alignment: Alignment) -> some View {
-        self
-            .frame(maxHeight: .infinity, alignment: alignment)
-    }
-    
-    // MARK: Custom Border View With Padding
-    func border(_ width: CGFloat, _ color: Color) -> some View {
-        self
-            .padding(.horizontal, 15)
-            .padding(.vertical, 10)
-            .background {
-                RoundedRectangle(cornerRadius: 5, style: .continuous)
-                    .stroke(color, lineWidth: width )
-            }
-    }
-    
-    // MARK: Custom Fill View With Padding
-    func fillView(_ color: Color) -> some View {
-        self
-            .padding(.horizontal, 15)
-            .padding(.vertical, 10)
-            .background {
-                RoundedRectangle(cornerRadius: 5, style: .continuous)
-                    .fill(color)
-            }
     }
 }
